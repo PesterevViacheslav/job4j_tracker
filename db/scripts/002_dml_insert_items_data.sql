17d3a65af774eeba1756a36a381ee0dcb2e269c0
ViacheslavPesterev <pesterevvv@gmail.com>
2022-11-05 00:14:08 +0300

    Решение задач уровня Middle. Категория : 3.3. Hibernate Тема : 3.3.2. Конфигурирование 2. HQL [#6874]

---
+++ db/scripts/002_dml_insert_items_data.sql
@@ -0,0 +1,3 @@
+INSERT INTO item (item_name, item_description, created) VALUES ('Item 1', 'dsc1', '2022-08-24 15:59:57')
+                                                              ,('Item 2', 'dsc2', '2023-08-24 15:59:57')
+                                                              ,('Item 3', 'dsc3', '2024-08-24 15:59:57');
